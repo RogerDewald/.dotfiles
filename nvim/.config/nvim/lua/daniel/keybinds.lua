cb284a4eed956fb0a8e6a859441d91a060553a4d
RogerDewald <daniel.r.dewald@gmail.com>
2024-09-29 19:05:32 -0500

    Adding my own take on tmux-sessionizer bc skill issue

--- nvim/.config/nvim/lua/daniel/keybinds.lua
+++ nvim/.config/nvim/lua/daniel/keybinds.lua
@@ -17,8 +17,6 @@ vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format)
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux-sessionizer<CR>")
-
 --My own keybinds for terminal and running code
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 
@@ -81,3 +79,7 @@ vim.keymap.set("n", "<C-q>", "")
 --Removing autocomplete box for text files
 vim.keymap.set("n", "<leader>ncmp", "<cmd>lua require('cmp').setup.buffer { enabled = false }<CR>")
 vim.keymap.set("n", "<leader>cmp", "<cmd>lua require('cmp').setup.buffer { enabled = true }<CR>")
+
+-- My own take on ThePrimeagen's tmux-sessionizer, bc I couldn't get his to 
+-- work for me
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/scripts/tmux-sessionizer<CR>")
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -36,3 +36,7 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 . "$HOME/.cargo/env"
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+export PATH="$HOME/.local/scripts:$PATH"
+
+bindkey -s ^f "~/.local/bin/scripts/tmux-sessionizer\n"
